69cfaa22c92c04e1a62acd2d379fc63422846046
tetema <tetema@student.wethinkcode.co.za>
2021-05-26 19:57:33 +0200

    Removed the API key.

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ fetchTemperature = async (city) => {
 
     const config = {
         method: 'get',
-        url: `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=011f9ade363009c9133ac05f4226b4f7`
+        url: `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=00000000000000000000000000000000`
     }
     let res = await axios(config);
 
